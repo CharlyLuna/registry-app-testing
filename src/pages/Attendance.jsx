e97893be77f52f616b564919ab11ee9c67acc73a
CharlyLuna <cluna10@ucol.mx>
2023-04-24 20:45:10 -0600

    Modify styles of attendance table

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -11,27 +11,27 @@ export const Attendance = () => {
         Alumnos registrados
       </h1>
       <hr />
-      <table className='table-auto'>
+      <table className='table-auto m-auto w-1/2 justify-center self-center items-center'>
         <thead>
-          <tr className='flex gap-10 justify-between mt-2 mb-2'>
-            <th>Nombre</th>
-            <th>Apellido paterno</th>
-            <th>Apellido materno</th>
-            <th>Grado y grupo</th>
-            <th>Asistencias</th>
+          <tr className='flex gap-10 justify-center mt-2 mb-2'>
+            <th className='w-full m-auto'>Nombre</th>
+            <th className='w-full m-auto'>Apellido paterno</th>
+            <th className='w-full m-auto'>Apellido materno</th>
+            <th className='w-full m-auto'>Grado y grupo</th>
+            <th className='w-full m-auto'>Asistencias</th>
           </tr>
         </thead>
         <tbody>
           {registeredStudents.map((student, index) => (
             <tr
               key={`${student.accountNumber}_${index}`}
-              className='flex mt-2 mb-2'
+              className='flex justify-center mt-2 mb-2 gap-10'
             >
-              <td className='w-8 m-auto'>{student.name}</td>
-              <td className='w-8 m-auto'>{student.fLastName}</td>
-              <td className='w-8 m-auto'>{student.sLastName}</td>
-              <td className='w-8 m-auto'>{student.gradeAndGroup}</td>
-              <td className='w-8 m-auto'>{attendances[index]}</td>
+              <td className='w-full text-center'>{student.name}</td>
+              <td className='w-full text-center'>{student.fLastName}</td>
+              <td className='w-full text-center'>{student.sLastName}</td>
+              <td className='w-full text-center'>{student.gradeAndGroup}</td>
+              <td className='w-full text-center'>{attendances[index]}</td>
             </tr>
           ))}
         </tbody>
